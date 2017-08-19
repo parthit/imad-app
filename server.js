27958a27876d6772247dc6211cd5d9b2b15a92b9
parthit <30855394+parthit@users.noreply.github.com>
2017-08-20 05:24:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/work', function(req, res){
+app.get('/article-one', function(req, res){
     res.send('works and you wont die');
 });
 
+app.get('/article-two', function(req, res){
+    res.send('works and you wont die');
+});
+
+app.get('/article-three', function(req, res){
+    res.send('works and you wont die');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
