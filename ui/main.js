1b6df3da88d5dd5126f70d81dbf47a0053ad7b2b
parthit <30855394+parthit@users.noreply.github.com>
2017-08-20 11:47:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerHTML = 'Naya text daala bhaiiii woh bhi saala java script mai !';
 var marginLeft = 0;
 
 function moveRight () {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 
@@ -17,7 +17,7 @@ function moveRight () {
 var img = document.getElementById('madi');
 img.onclick = function () {
     
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
     
     
 };
