c5d75b29bc3b86ffed3538b53dfd912eb1cc0e73
parthit <30855394+parthit@users.noreply.github.com>
2017-08-20 11:35:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,12 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'Naya text daala bhaiiii woh bhi saala java script mai !';
+
+
+//moving the image
+
+var img = document.getElementById('fotu');
+img.onclick = function () {
+    img.style.marginLeft = '100 px';
+    
+};
