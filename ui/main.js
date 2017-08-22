6d13293a3ef0bb37b1d9e90c3261ede60071c400
parthit <30855394+parthit@users.noreply.github.com>
2017-08-22 14:47:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'Naya text daala bhaiiii woh bhi saala java script mai !';
-
+element.innerHTML = 'This is a second line that I have added in java script.';
 
 //moving the image
 var marginLeft = 0;
