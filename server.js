2acfaba29676c79b9ab848cb4fb351d46a6eb0dd
parthit <30855394+parthit@users.noreply.github.com>
2017-08-23 18:32:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,9 +124,27 @@ app.get('/test-db', function (req, res){
     });
 });
 
-app.get('/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res){
     var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+    
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err, result){
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else{
+            if(result.row.length === 0){
+                res.status(404).send('Article not found');
+            }
+            else{
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    
+    
+    res.send(createTemplate(articleData));
 });
 
 
