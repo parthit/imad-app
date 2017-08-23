708a86414f6ccc74ed2702f419b598c19bb0d91e
parthit <30855394+parthit@users.noreply.github.com>
2017-08-23 18:52:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.get('/articles/:articleName', function(req, res){
             res.status(500).send(err.toString());
         }
         else{
-            if(result.row.length === 0){
+            if(result.rows.length === 0){
                 res.status(404).send('Article not found');
             }
             else{
